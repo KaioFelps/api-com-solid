d2b1c5127c51c9a7bcf1b1b65a51f6c94e0bf972
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-05-22 18:31:49 -0300

    :wrench: configs eslint and eslint ignore

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { app } from "./app";
+import { env } from "./env";
 
-app.listen({
-    port: 3333,
+app
+  .listen({
+    port: env.PORT,
     host: "0.0.0.0",
-}).then(() => console.log("🎊 HTTP server is running!"))
+  })
+  .then(() => console.log("🎊 HTTP server is running!"));
