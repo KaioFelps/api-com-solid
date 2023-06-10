b15482b1f58a750e74f951eeae50ca9203ae6be9
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-06-10 13:44:10 -0300

    implements find checkin by userid and date in checkin repositories

--- src/repositories/in-memory/in-memory-check-ins.repository.ts
+++ src/repositories/in-memory/in-memory-check-ins.repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, CheckIn } from "@prisma/client";
 import { CheckInsRepositoryInterface } from "../check-ins-repository";
 import { randomUUID } from "node:crypto";
+import dayjs from "dayjs";
 
 export class InMemoryCheckInsRepository implements CheckInsRepositoryInterface {
   public items: CheckIn[] = [];
@@ -16,5 +17,22 @@ export class InMemoryCheckInsRepository implements CheckInsRepositoryInterface {
 
     this.items.push(checkIn);
     return checkIn;
+  }
+
+  async findByUserIdOnDate(
+    userId: string,
+    date: Date
+  ): Promise<CheckIn | null> {
+    const startOfTheDay = dayjs(date).startOf("day");
+    const endOfTheDay = dayjs(date).endOf("day");
+
+    const checkInOnGivenDate = this.items.find(
+      (checkin) =>
+        checkin.user_id === userId &&
+        dayjs(checkin.created_at).isAfter(startOfTheDay) &&
+        dayjs(checkin.created_at).isBefore(endOfTheDay)
+    );
+
+    return checkInOnGivenDate ?? null;
   }
 }
