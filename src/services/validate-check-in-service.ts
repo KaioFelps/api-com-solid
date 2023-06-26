fd20fc5fec2e67191b4156a98635610e0ee25ee6
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-06-26 19:11:59 -0300

    implements the twenty minutes check in lifetime business rule at validate-check-in-service + test

--- src/services/validate-check-in-service.ts
+++ src/services/validate-check-in-service.ts
@@ -1,6 +1,8 @@
 import { CheckInsRepositoryInterface } from "@/repositories/check-ins-repository";
 import { CheckIn } from "@prisma/client";
 import { NotFoundError } from "./errors/not-found-error";
+import dayjs from "dayjs";
+import { ExpiredCheckInsError } from "./errors/expired-check-ins-error";
 
 interface ValidateCheckInsInput {
   checkInId: string;
@@ -20,6 +22,18 @@ export class ValidateCheckIns {
 
     if (!checkIn) {
       throw new NotFoundError();
+    }
+
+    const currentDate = dayjs(new Date());
+    const checkInCreationDate = dayjs(checkIn.created_at);
+
+    const diffInMsBetweenNowAndCheckInCreationDate =
+      currentDate.diff(checkInCreationDate);
+
+    const TwentyMinutesInMs = 1000 * 60 * 20;
+
+    if (diffInMsBetweenNowAndCheckInCreationDate > TwentyMinutesInMs) {
+      throw new ExpiredCheckInsError();
     }
 
     checkIn.validated_at = new Date();
--- src/services/validate-check-in.test.ts
+++ src/services/validate-check-in.test.ts
@@ -1,8 +1,9 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
 import { ValidateCheckIns } from "./validate-check-in-service";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins.repository";
 import { CheckInsRepositoryInterface } from "@/repositories/check-ins-repository";
 import { NotFoundError } from "./errors/not-found-error";
+import { ExpiredCheckInsError } from "./errors/expired-check-ins-error";
 
 let checkInsRepository: CheckInsRepositoryInterface;
 let sut: ValidateCheckIns;
@@ -11,6 +12,12 @@ describe("Validate Check Ins Service", () => {
   beforeEach(async () => {
     checkInsRepository = new InMemoryCheckInsRepository();
     sut = new ValidateCheckIns(checkInsRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(async () => {
+    vi.useRealTimers();
   });
 
   it("should validate a check in", async () => {
@@ -34,5 +41,23 @@ describe("Validate Check Ins Service", () => {
     await expect(invalidValidationPromise).rejects.toBeInstanceOf(
       NotFoundError
     );
+  });
+
+  it("should not validate a check-in after 20 minutes of creation", async () => {
+    vi.setSystemTime(new Date(2023, 5, 26, 18, 49, 0, 0));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const TwentyOneMinutes = 1000 * 60 * 21;
+    vi.advanceTimersByTime(TwentyOneMinutes);
+
+    const validateCheckInPromise = sut.execute({
+      checkInId: createdCheckIn.id,
+    });
+
+    expect(validateCheckInPromise).rejects.toBeInstanceOf(ExpiredCheckInsError);
   });
 });
