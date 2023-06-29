98510132e24d9e78f8ac2b3007ecaca66b1f1b95
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-06-29 19:22:54 -0300

    configs vitest's prisma database environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+
+const prisma = new PrismaClient();
+
+function generateDatabaseUrl(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please, provide a database url.");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("search", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   async setup() {
     // código executado antes de cada ARQUIVO DE TESTE
-    console.log("Setup");
+    const newSchema = randomUUID();
+    const newDatabaseUrl = generateDatabaseUrl(newSchema);
+    process.env.DATABASE_URL = newDatabaseUrl;
+
+    // o deploy serve para que o prisma não compare o banco de dados, pois o deploy pula essa etapa, diferente do dev
+    execSync("npx prisma migrate deploy");
 
     return {
       // após os testes
-      teardown() {
-        console.log("Teardown");
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${newSchema}" CASCADE` // CASCADE faz com que, caso alguma informação dependa desse schema, ela também seja apagada. Isso vai apagar também os índices e chaves primárias.
+        );
+
+        await prisma.$disconnect();
       },
     };
   },
