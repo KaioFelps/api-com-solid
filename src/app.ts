a825b75737b2adef5ce4ce7f99c7a2dc933be2ee
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-02 00:22:29 -0300

    registers gym and check-ins routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,13 +4,15 @@ import { GlobalErrorHandler } from "./services/errors/global-error-handler";
 import fastifyJwt from "@fastify/jwt";
 import { env } from "./env";
 import { gymsRoutes } from "./http/controllers/gyms/routes";
+import { checkInsRoutes } from "./http/controllers/check-ins/routes";
 
 export const app = fastify();
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
 });
 
-app.register(gymsRoutes);
+app.register(checkInsRoutes);
 app.register(usersRoutes);
+app.register(gymsRoutes);
 
 app.setErrorHandler(GlobalErrorHandler);
