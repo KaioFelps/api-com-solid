1459776d2d1ccf0bdf2aebab9ad41b4563b296d0
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-02 12:41:06 -0300

    creates refreshToken and sends it to request response as a https safe cookie

--- src/http/controllers/users/authenticate.controller.ts
+++ src/http/controllers/users/authenticate.controller.ts
@@ -15,6 +15,7 @@ export async function authenticate(req: FastifyRequest, rep: FastifyReply) {
     const authenticateService = new AuthenticateFactory().execute();
 
     const { user } = await authenticateService.execute({ email, password });
+
     const token = await rep.jwtSign(
       {},
       {
@@ -24,9 +25,38 @@ export async function authenticate(req: FastifyRequest, rep: FastifyReply) {
       }
     );
 
-    return rep.status(200).send({
-      token,
-    });
+    const refreshToken = await rep.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+          /*
+           * expira em 7 dias
+           * um novo token é gerado a cada 10 minutos
+           * para perder o token, o usuário precisa ficar 7 dias sem fazer nenhuma requisição para a aplicação
+           */
+          expiresIn: "7d",
+        },
+      }
+    );
+
+    return rep
+      .status(200)
+      .setCookie("refreshToken", refreshToken, {
+        // disponibiliza esse token apenas no pacote http, não fica disponível no browser e, logo, não fica acessível para o front-end ou javascript.
+        httpOnly: true,
+        // rotas do dominio que poderão ter acesso ao cookie
+        // / quer dizer que todo o back-end pode ler o valor do cookie
+        path: "/",
+        // diz se vai ser encriptado pelo https ou não
+        // se sim, o back-end não vai ter acesso ao valor bruto em string do cookie
+        secure: true,
+        // faz com que só seja acessível para o back-end do mesmo site
+        sameSite: true,
+      })
+      .send({
+        token,
+      });
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return rep.status(400).send({ message: err.message });
