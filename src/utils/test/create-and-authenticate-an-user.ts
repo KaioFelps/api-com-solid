ad470f0e0cf6a5bbe40515adacdddb02bb5e59b5
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-02 14:25:01 -0300

    create and authenticate an user util function now asks whether user is admin or not

--- src/utils/test/create-and-authenticate-an-user.ts
+++ src/utils/test/create-and-authenticate-an-user.ts
@@ -1,11 +1,21 @@
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
 import { FastifyInstance } from "fastify";
 import request from "supertest";
 
-export async function createAndAuthenticateAnUser(app: FastifyInstance) {
-  await request(app.server).post("/users").send({
-    name: "Kaio Felps",
-    email: "kaiofelps@dev.com",
-    password: "123456",
+export async function createAndAuthenticateAnUser(
+  app: FastifyInstance,
+  isAdmin: boolean = false
+) {
+  const hashingRounds = 4;
+
+  await prisma.user.create({
+    data: {
+      name: "Kaio Felps",
+      email: "kaiofelps@dev.com",
+      password_hash: await hash("123456", hashingRounds),
+      role: isAdmin ? "ADMIN" : "MEMBER",
+    },
   });
 
   const authResponse = await request(app.server).post("/sessions").send({
