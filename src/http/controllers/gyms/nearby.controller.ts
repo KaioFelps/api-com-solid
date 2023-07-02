a03feaaca5a16cf83f450c9a3f2cceb28ad498da
Kaio Felipe <kaiofelipe2005@gmail.com>
2023-07-02 00:21:25 -0300

    get nearby gyms controller + test

--- src/http/controllers/gyms/nearby.controller.ts
+++ src/http/controllers/gyms/nearby.controller.ts
@@ -3,18 +3,18 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
 export async function nearby(req: FastifyRequest, rep: FastifyReply) {
-  const nearbyGymsBodySchema = z.object({
-    latitude: z.number().refine((value) => {
+  const nearbyGymsQuerySchema = z.object({
+    latitude: z.coerce.number().refine((value) => {
       // a latitude precisa ser sempre maior ou igual a 90 (positivo ou negativo)
       return Math.abs(value) <= 90;
     }),
-    longitude: z.number().refine((value) => {
+    longitude: z.coerce.number().refine((value) => {
       // a latitude precisa ser sempre maior ou igual a 180 (positivo ou negativo)
       return Math.abs(value) <= 180;
     }),
   });
 
-  const { latitude, longitude } = nearbyGymsBodySchema.parse(req.body);
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(req.query);
 
   const fetchNearbyGymsService = new FetchNearbyGymsFactory().execute();
 
---
+++ src/http/controllers/gyms/nearby.test.ts
@@ -0,0 +1,58 @@
+import request from "supertest";
+import { app } from "@/app";
+import { createAndAuthenticateAnUser } from "@/utils/test/create-and-authenticate-an-user";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+
+describe("Nearby Gym (e2e)", async () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should find nearby gyms", async () => {
+    const { token } = await createAndAuthenticateAnUser(app);
+
+    await request(app.server)
+      .post("/gyms/create")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        latitude: -24.0112631,
+        longitude: -52.3660119,
+        title: "Ruby Gym",
+        description: "",
+        phone: "44999999999",
+      });
+
+    await request(app.server)
+      .post("/gyms/create")
+      .set("Authorization", `Bearer ${token}`)
+      .send({
+        latitude: -24.04701,
+        longitude: -52.414592,
+        title: "Rust Gym",
+        description: "",
+        phone: "44999999999",
+      });
+
+    const response = await request(app.server)
+      .get("/gyms/nearby")
+      .query({
+        latitude: -24.04701,
+        longitude: -52.414592,
+      })
+      .set("Authorization", `Bearer ${token}`)
+      .send()
+      .expect(200);
+
+    expect(response.body.gyms).toHaveLength(1);
+
+    expect(response.body.gyms).toEqual([
+      expect.objectContaining({
+        title: "Rust Gym",
+      }),
+    ]);
+  });
+});
